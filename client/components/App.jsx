ea218bc0435add74400ba9443471156a413e673a
Toan Tran <trantoankk@gmail.com>
2020-04-30 15:03:19 -0700

    Setup webpack, react, eslint BnB environment

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h4> Hello from React</h4>
+      </div>
+    )
+  }
+}
+export default App;
