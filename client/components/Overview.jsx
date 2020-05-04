0395e1c1aedf712fedbcdb9b08bdafaaa0853536
Toan Tran <trantoankk@gmail.com>
2020-05-04 14:31:00 -0700

    setup the basic structure of Overview section

--- client/components/Overview.jsx
+++ client/components/Overview.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const ReviewSection = styled.section`
+  border: 1px solid black;
+  border-radius: 1px;
+  height: 100px;
+  width: 600px;
+  text-color: blue;
+`;
+
+const ReviewHeader = styled.h1`
+  color: blue;
+  font-size: 1.5em;
+`;
+
+const StatusGrid = styled.section`
+  display: grid;
+  grid-template-columns: 3fr 7fr;
+`;
+
+const TotalScores = styled.div`
+  display: flex;
+`;
+
+const SearchReview = styled.form`
+  border: 1px solid gray;
+  border-radius: 2px;
+`;
 
 const Overview = (props) => {
 
   return (
-    <div>
-      <h4> Reviews</h4>
-      <div>
-        <div>
-
+    <ReviewSection>
+      <ReviewHeader> Reviews </ReviewHeader>
+      <StatusGrid>
+        <TotalScores>
           <div>
             <span>starImg</span>
             <span>#</span>
@@ -17,17 +45,14 @@ const Overview = (props) => {
             <span>#</span>
             <span> Reviews</span>
           </div>
-
-          <form>
+        </TotalScores>
+        <SearchReview>
             <input type="text" placeholder="Search Reviews" name="seach" value=""/>
-            <button> <img src=""/> </button>
-            <butotn type="submit'"> <img src=''/></butotn>
-          </form>
-
-        </div>
-
-      </div>
-    </div>
+            <button> <img src=""/>X</button>
+            <button type="submit'"> <img src=''/></button>
+        </SearchReview>
+      </StatusGrid>
+    </ReviewSection>
   )
 }
 
