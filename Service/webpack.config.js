1df51a547325661e387a731548e8073f1686c7b3
Toan Tran <trantoankk@gmail.com>
2020-05-16 10:30:40 -0700

    refactored webpack to support dev env

--- Service/webpack.config.js
+++ Service/webpack.config.js
@@ -8,6 +8,7 @@ const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
+  mode: 'development',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
@@ -28,34 +29,12 @@ module.exports = {
       },
     ],
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'async',
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 6,
-      maxInitialRequests: 4,
-      automaticNameDelimiter: '~',
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true
-        }
-      }
-    }
-  },
   plugins: [
-    new webpack.DefinePlugin({ //<--key to reduce React's size
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-    }),
+    // new webpack.DefinePlugin({ //<--key to reduce React's size
+    // 'process.env': {
+    //   'NODE_ENV': JSON.stringify('production')
+    // }
+    // }),
     new CompressionPlugin({
       filename: '[path].br[query]',
       algorithm: 'brotliCompress',
